1c3f2b4de8633839fe9e8d949347f3eeca2a4cdd
7N-yyq <60874841+7N-yyq@users.noreply.github.com>
2020-11-07 18:35:41 +0800

    Add files via upload

---
+++ buttonListener.java
@@ -0,0 +1,11 @@
+package something;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class buttonListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ test.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class test{
+
+    public static void main(String[] args){
+        int b = 1;
+        System.out.println(Integer.toBinaryString(b));
+    }
+
+}
